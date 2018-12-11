d1866665c291c6709c11f2457c81b8ca61b230b8
zhangjiang123 <1014775214@qq>
2018-12-11 11:25:51 +0800

    添加新工程

---
+++ git/git/src/com/example/git/GitDemo.java
@@ -0,0 +1,13 @@
+package com.example.git;
+
+public class GitDemo {
+
+    public static void main(String[] args) {
+
+        System.out.println("1");
+        System.out.println("2");
+        System.out.println("3");
+
+    }
+
+}
